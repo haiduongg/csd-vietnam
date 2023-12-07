f5e079bb35628e4dfc5ed8568c13009635c0c6d4
Cao Hai Duong <46746869+haiduongg@users.noreply.github.com>
2023-12-07 20:35:01 +0700

    update Category Gallerry

--- src/components/Category/CategoryGallery.jsx
+++ src/components/Category/CategoryGallery.jsx
@@ -4,12 +4,11 @@ import { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { motion } from 'framer-motion';
 
 export default function CategoryGallery({ dataCategory }) {
   const [currentImage, setCurrentImage] = useState({ img: null, i: 0 });
-  const viewImage = (image, index) => {
-    setCurrentImage({ img: image, i: index });
-  };
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className='relative'>
       <div className='heading container mt-8 mb-6'>
@@ -20,46 +19,60 @@ export default function CategoryGallery({ dataCategory }) {
           This is category {dataCategory.name} image gallery.
         </p>
       </div>
-      {currentImage.i && (
-        <div className='fixed top-0 w-[100vw] h-[100vh] overflow-hidden flex justify-between items-center bg-dark z-50 overflow-y-hidden'>
+      {openModal && (
+        <div className='overflow-hidden fixed top-0 w-full h-full flex justify-between items-center bg-dark z-50'>
+          {/* Close Btn */}
           <div
             className='absolute top-4 right-4'
-            onClick={() => setCurrentImage({ img: null, i: 0 })}
+            onClick={() => setOpenModal(false)}
           >
-            <Button>
+            <Button className='rounded-full bg-primary-900 hover:bg-primary-800'>
               <IoMdClose size={20} />
             </Button>
           </div>
-          <div>
+
+          {/* Next Btn 
+          <div className='fixed left-3'>
             <Button>
               <FaChevronLeft size={20} />
             </Button>
+          </div> */}
+
+          {/* Image */}
+          <div className='w-full h-full flex items-center justify-center'>
+            <img
+              src={currentImage.img}
+              alt={`image`}
+              className='max-w-[90%] max-h-[90%] cursor-zoom-out'
+              onClick={() => setOpenModal(false)}
+            />
           </div>
-          <img
-            src={currentImage.img}
-            alt={`image`}
-            className='h-[90%] w-[90%]'
-          />
-          <div>
+
+          {/* Previous Btn 
+          <div className='fixed right-3'>
             <Button>
               <FaChevronRight size={20} />
             </Button>
-          </div>
+          </div> */}
         </div>
       )}
-      <div>
+      <div className='mx-24'>
         {dataCategory.images && (
           <ResponsiveMasonry
             columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
           >
             <Masonry gutter='20px'>
               {dataCategory.images?.map((image, index) => (
-                <img
+                <motion.img
                   key={index}
                   src={image}
                   alt={`image ${index}`}
-                  className='w-full block shadow-lg cursor-pointer'
-                  onClick={() => viewImage(image, index)}
+                  className='w-full block shadow-lg cursor-zoom-in'
+                  onClick={() => {
+                    setCurrentImage({ img: image, i: index });
+                    setOpenModal(true);
+                  }}
+                  whileHover={{ scale: 1.04 }}
                 />
               ))}
             </Masonry>
